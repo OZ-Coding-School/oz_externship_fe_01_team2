a440637bfd6ac326308efe94d3a5e410d93e78a9
soyeon1351 <pack6411351@gmail.com>
2025-06-20 11:32:37 +0900

    ✨feat: signup 라우팅 설정(#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import SignUpPage from './pages/SignUpPage'
+import { Routes, Route } from 'react-router'
 
 function App() {
   return (
-    <div>
-      <SignUpPage />
-    </div>
+    <Routes>
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
   ) 
 }
+
+
 
 export default App
