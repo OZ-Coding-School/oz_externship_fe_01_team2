2d34cca098cd7134ddc002ae13d0c8e991574a18
jngmnj <exorbitance@naver.com>
2025-06-25 15:26:26 +0900

    💡 chore: App.css import문 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
-import MyPage from './pages/MyPage'
-import LoginPage from './pages/LoginPage'
 import HomePage from './pages/index'
-import './App.css'
 
 function App() {
   return (
@@ -12,8 +9,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<HomePage />} />
-            <Route path="LoginPage" element={<LoginPage />} />
-            <Route path="MyPage" element={<MyPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
