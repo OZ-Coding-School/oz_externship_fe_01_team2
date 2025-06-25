1a86453565670451284b3302a63a02f5c6c20e6c
ella6010 <ella6010@gmail.com>
2025-06-25 11:49:59 +0900

    ✨ feat: 기본라우팅(#38)
     기본라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import MyPage from './pages/MyPage'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/index'
 import './App.css'
 
 function App() {
-  return <div>template</div>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="LoginPage" element={<LoginPage />} />
+            <Route path="MyPage" element={<MyPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App
