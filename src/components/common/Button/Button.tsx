343031c514343a65f88473817e76f798a987c6ba
Y.KangHyeok <heavenin24@naver.com>
2025-06-30 17:51:14 +0900

    refactor: button, modal 컴포넌트cn 유틸리티적용, 불필요한 props정리
    - classname prop 추가로 확장성 추가
    - 사용하지않는 props 제거(radius, width, height, fontsize)
    - tailwind 색상정의 사용
    
    관련이슈: #64

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,46 +1,39 @@
 //src/components/common/Button/Button.tsx
 import React from 'react'
+import { cn } from '../../../utils/cn'
 import type { ButtonProps } from './Button.types'
 
 const Button: React.FC<ButtonProps> = ({
   variant = 'fill',
   disabled = false,
+  className,
   children,
   onClick,
-  radius = '4px',
-  width = '156px',
-  height = '48px',
-  fontSize = '16px',
 }) => {
-  const baseStyles = 'font-semibold focus:outline-none transition-colors'
-
   const variantMap = {
-    fill: 'bg-[rgba(98,1,224,1)] text-[rgba(255,255,255,1)] hover:bg-[rgba(78,1,179,1)] active:bg-[rgba(59,1,134,1)]',
+    fill: 'bg-primary text-white hover:bg-primary-600 active:bg-primary-700',
     outline:
-      'bg-[rgba(236,236,236,1)] border border-[rgba(206,206,206,1)] text-[rgba(77,77,77,1)] active:bg-[rgba(239,230,252,1)] active:border-[rgba(98,1,224,1)] active:text-[rgba(98,1,224,1)]',
-    ghost:
-      'bg-[rgba(250,250,250,1)] text-[rgba(98,1,224,1)] hover:bg-[rgba(236,236,236,1)] active:bg-[rgba(189,189,189,1)]',
+      'bg-gray-200 border border-gray-250 text-gray-600 active:bg-primary-100 active:border-primary active:text-primary',
+    ghost: 'bg-white text-primary hover:bg-gray-200 active:bg-gray-250',
     check:
-      'bg-[rgba(239,230,252,1)] border border-[rgba(98,1,224,1)] text-[rgba(98,1,224,1)] active:bg-[rgba(236,236,236,1)] active:border-[rgba(206,206,206,1)] active:text-[rgba(77,77,77,1)]',
+      'bg-primary-100 border border-primary text-primary active:bg-gray-200 active:border-gray-250 active:text-gray-600',
   }
 
   const getStyles = () => {
     if (disabled && variant === 'ghost') {
-      return 'bg-[rgba(236,236,236,1)] text-[rgba(189,189,189,1)] cursor-not-allowed'
+      return 'bg-gray-200 text-gray-250 cursor-not-allowed'
     }
     return variantMap[variant] || variantMap.fill
   }
 
   return (
     <button
-      className={`${baseStyles} ${getStyles()}
-      ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
-      style={{
-        borderRadius: radius,
-        width: width,
-        height: height,
-        fontSize: fontSize,
-      }}
+      className={cn(
+        'font-semibold focus:outline-none transition-colors rounded-sm py-3 px-12',
+        getStyles(),
+        disabled ? 'cursor-not-allowed' : 'cursor-pointer',
+        className
+      )}
       disabled={disabled}
       onClick={disabled ? undefined : onClick}
     >
@@ -48,5 +41,4 @@ const Button: React.FC<ButtonProps> = ({
     </button>
   )
 }
-
 export default Button
---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,40 @@
+import { X } from 'lucide-react'
+import React from 'react'
+import { cn } from '../../utils/cn'
+
+interface ModalProps {
+  isOpen: boolean
+  className?: string
+  onClose: () => void
+  children: React.ReactNode
+}
+
+const Modal = ({ isOpen, className, onClose, children }: ModalProps) => {
+  if (!isOpen) return null
+
+  return (
+    <div
+      className={cn(
+        `fixed inset-0 bg-black/60 flex items-center justify-center z-50`,
+        className
+      )}
+      onClick={onClose}
+    >
+      <div
+        className="bg-white rounded-[12px] shadow-xl mt-[280px] mb-[278px] p-[24px] gap-[10px] relative"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-6 right-6 text-gray-400 transition hover:text-gray-600 text-xl cursor-pointer"
+          aria-label="닫기"
+        >
+          <X />
+        </button>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Modal
