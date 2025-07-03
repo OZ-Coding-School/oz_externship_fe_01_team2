8b2e7b7512789fcb35abc8e9a8a5697011d388ab
ella6010 <ella6010@gmail.com>
2025-07-03 23:03:28 +0900

    🐛fix: 질의응답 리스트 기능 정리(#74)
     QnaFilterModal 코드 수정, QnaCard 타입분리 등 등

---
+++ src/components/qna/category.ts
@@ -0,0 +1,16 @@
+export const CATEGORY = {
+  프론트엔드: {
+    '프로그래밍 언어': ['JavaScript', 'TypeScript'],
+    '웹 프레임워크': ['React', 'Vue'],
+  },
+  백엔드: {
+    '프로그래밍 언어': ['Python', 'Java'],
+    '웹 프레임워크': ['Django', 'FastAPI'],
+  },
+}
+
+export const DEFAULT_CATEGORY = {
+  main: '대분류',
+  sub: '중분류',
+  detail: '소분류',
+}
---
+++ src/types/qnaCard.types.ts
@@ -0,0 +1,13 @@
+export type QuestionCard = {
+  id: number
+  title: string
+  content: string
+  category: string
+  subCategory: string
+  language: string
+  answerCount: number
+  viewCount: number
+  nickname: string
+  time: string
+  thumbnail?: string
+}
