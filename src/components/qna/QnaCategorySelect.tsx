01b9da1b6a1db69ea2d9dbee3e23485e4b7b29d9
Y.KangHyeok <heavenin24@naver.com>
2025-07-04 17:03:28 +0900

    ♻️ refactor: mock데이터카테코리(중첩으로 변환), 3단 카테고리 구현(#12) 변경 사항 : mock데이터 카테고리중첩으로 변경하고 3단 카테고리 보기쉬운 코드로 변환

--- src/components/qna/QnaCategorySelect.tsx
+++ src/components/qna/QnaCategorySelect.tsx
@@ -1,78 +1,88 @@
-// src/components/qna/QnaCategorySelect.tsx
-import SingleDropdown from '../common/SingleDropdown'
-import { mockCategories } from '../../components/Mocks/MockCategories'
+import SingleDropdown from '../../components/common/SingleDropdown'
+import { mockCategories } from '../Mocks/MockCategories'
+import { useState } from 'react'
 
-interface Props {
-  majorId: number | null
-  setMajorId: (id: number | null) => void
-  middleId: number | null
-  setMiddleId: (id: number | null) => void
-  minorId: number | null
-  setMinorId: (id: number | null) => void
+interface SelectedCategories {
+  major: string | null
+  middle: string | null
+  minor: string | null
 }
 
-export default function QnaCategorySelect({
-  majorId,
-  setMajorId,
-  middleId,
-  setMiddleId,
-  minorId,
-  setMinorId,
-}: Props) {
-  // 대분류, 중분류, 소분류 옵션 추출 (타입 추론 O)
-  const majorOptions = mockCategories.filter(
-    (cat) => cat.category_type === 'major'
-  )
-  const middleOptions = mockCategories.filter(
-    (cat) => cat.category_type === 'middle' && cat.parent_id === majorId
-  )
-  const minorOptions = mockCategories.filter(
-    (cat) => cat.category_type === 'minor' && cat.parent_id === middleId
-  )
+export default function QnaCategorySelect() {
+  const [selectedCategories, setSelectedCategories] =
+    useState<SelectedCategories>({
+      major: null,
+      middle: null,
+      minor: null,
+    })
+
+  // 대분류 옵션
+  const majorOptions = mockCategories.map((item) => item.name)
+
+  // 중분류 옵션 (추가!)
+  let middleOptions: string[] = []
+  if (selectedCategories.major) {
+    const selectedMajor = mockCategories.find(
+      (item) => item.name === selectedCategories.major
+    )
+    if (selectedMajor) {
+      middleOptions = selectedMajor.child_categories.map((child) => child.name)
+    }
+  }
+
+  // 소분류 옵션 (추가!)
+  let minorOptions: string[] = []
+  if (selectedCategories.middle) {
+    const selectedMajor = mockCategories.find(
+      (item) => item.name === selectedCategories.major
+    )
+    if (selectedMajor) {
+      const selectedMiddle = selectedMajor.child_categories.find(
+        (child) => child.name === selectedCategories.middle
+      )
+      if (selectedMiddle) {
+        minorOptions = selectedMiddle.child_categories.map(
+          (child) => child.name
+        )
+      }
+    }
+  }
 
   return (
     <div className="grid grid-cols-3 gap-4 mb-6">
-      {/* 대분류 */}
       <SingleDropdown
-        options={['대분류 선택', ...majorOptions.map((cat) => cat.name)]}
-        selected={
-          majorOptions.find((cat) => cat.id === majorId)?.name || '대분류 선택'
-        }
-        onChange={(value) => {
-          const found = majorOptions.find((cat) => cat.name === value)
-          setMajorId(found?.id ?? null)
-          setMiddleId(null)
-          setMinorId(null)
-        }}
+        options={majorOptions}
         placeholder="대분류 선택"
-      />
-      {/* 중분류 */}
-      <SingleDropdown
-        options={['중분류 선택', ...middleOptions.map((cat) => cat.name)]}
-        selected={
-          middleOptions.find((cat) => cat.id === middleId)?.name ||
-          '중분류 선택'
-        }
-        onChange={(value) => {
-          const found = middleOptions.find((cat) => cat.name === value)
-          setMiddleId(found?.id ?? null)
-          setMinorId(null)
+        onChange={(selected) => {
+          setSelectedCategories({
+            major: selected,
+            middle: null,
+            minor: null,
+          })
         }}
+      />
+      <SingleDropdown
+        options={middleOptions}
         placeholder="중분류 선택"
-        disabled={!majorId}
-      />
-      {/* 소분류 */}
-      <SingleDropdown
-        options={['소분류 선택', ...minorOptions.map((cat) => cat.name)]}
-        selected={
-          minorOptions.find((cat) => cat.id === minorId)?.name || '소분류 선택'
-        }
-        onChange={(value) => {
-          const found = minorOptions.find((cat) => cat.name === value)
-          setMinorId(found?.id ?? null)
+        onChange={(selected) => {
+          setSelectedCategories((prevCategories) => ({
+            ...prevCategories,
+            middle: selected,
+            minor: null,
+          }))
         }}
+        disabled={!selectedCategories.major}
+      />
+      <SingleDropdown
+        options={minorOptions}
         placeholder="소분류 선택"
-        disabled={!middleId}
+        onChange={(selected) => {
+          setSelectedCategories((prevCategories) => ({
+            ...prevCategories,
+            minor: selected,
+          }))
+        }}
+        disabled={!selectedCategories.middle}
       />
     </div>
   )
