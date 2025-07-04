6fefa033004ddf972658eb1b78bc1fd1c20608b3
jngmnj <exorbitance@naver.com>
2025-07-04 16:33:45 +0900

    ✨ feat: 에러 페이지 개발(#78)
    - 에러 코드 및 메시지 중앙 관리 시스템 도입
    - 라우트 일치 실패 시 404 Not Found 처리 기능 구현
    - 정의되지 않은 에러 코드에 대한 UNKNOWN 메시지 폴백 처리
    - 독립적인 에러 페이지 렌더링을 위한 레이아웃 분리

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from 'react-router-dom'
+import Button from '../components/common/Button'
+import { ERROR_MESSAGES } from '../constants/errorMessage'
+import { ErrorCode } from '../types/errorCodes'
+
+interface ErrorState {
+  code?: ErrorCode
+}
+
+const ErrorPage = () => {
+  const location = useLocation()
+  const state = location.state as ErrorState
+
+  const code: ErrorCode = state?.code || ErrorCode.NOT_FOUND
+
+  const getErrorMessage = (code: ErrorCode) => {
+    return ERROR_MESSAGES[code] || ERROR_MESSAGES[ErrorCode.UNKNOWN]
+  }
+  const { title, message, description } = getErrorMessage(code)
+
+  return (
+    <div className="min-h-[80vh] flex flex-col items-center justify-center">
+      <h1 className="text-9xl font-bold text-primary mb-4">{title}</h1>
+      <p className="text-xl text-gray-600 mb-2">{message}</p>
+      <p className="text-gray-500 mb-20">{description}</p>
+      <Button>
+        <Link to="/">홈으로 돌아가기</Link>
+      </Button>
+    </div>
+  )
+}
+
+export default ErrorPage
