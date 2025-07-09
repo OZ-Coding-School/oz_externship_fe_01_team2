5fe7d686de18bea34aa5eba173f82dcc9ee67522
Y.KangHyeok <heavenin24@naver.com>
2025-07-09 16:13:06 +0900

    ✨ feat: 질문등록페이지구현(#12) 변경 사항 : QnaCreatePage구현 및 라우터연동

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import SignUpPage from './pages/SignUpPage'
 import SignUpFormPage from './pages/SignUpFormPage'
 import ChangePasswordPage from '../src/pages/ChangePasswordPage'
 import QnaCreatePage from './pages/QnaCreatePage'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="signupform" element={<SignUpFormPage />} />
           <Route path="changePassword" element={<ChangePasswordPage />} />
           <Route path="qna/create" element={<QnaCreatePage />} />
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
